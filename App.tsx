f1b818db46fa1806eaa5e2b0acb152459de5e9b6
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-12-05 12:50:21 -0800

    refactored directory scheme

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from "./hooks/useCachedResources";
-import Navigation from "./navigation";
+import useCachedResources from "./src/hooks/useCachedResources";
+import Navigation from "./src/navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
